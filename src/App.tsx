df4910d38da011b362a5104c75e74716952cc8fe
Lucas <lukerdriscoll@gmail.com>
2024-01-26 17:33:36 -0500

    input validation added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import { ProjectSection } from './ProjectSection';
+import { ProjectSection, ProjectSectionProps } from './ProjectSection';
+import { ProjectCardProps } from './ProjectCard';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import Container from "react-bootstrap/Container"
+import Spinner from "react-bootstrap/Spinner"
+import Joi from "joi"
+const schema = Joi.array<ProjectSectionProps[]>().items(Joi.object<ProjectSectionProps>({
+  name: Joi.string().required(),
+  description: Joi.string().required(),
+  projects: Joi.array<ProjectCardProps[]>().items(Joi.object<ProjectCardProps>({
+    name: Joi.string().required(),
+    description: Joi.string().required(),
+    url: Joi.string().required(),
+    logo: Joi.string(),
+    demo_url: Joi.string()
+  }))
+}))
+
 function App() {
+
+  const [projects, setProjects] = useState<null|ProjectSectionProps[]>(null)
+
+  useEffect(() => {
+    let setState = true
+    fetch("/projects.json").then(r => {
+      r.json().then((js: unknown) => {
+        if(setState){
+          const project = schema.validate(js)
+          if(project.error){
+            alert(project.error)
+          }else{
+            setProjects(project.value)
+          }
+        }
+      })
+    })
+    return () => {
+      setState = false
+    }
+  },[])
+
   return (
     <div className="app">
       <h1>
@@ -14,59 +51,20 @@ function App() {
       <h2>
         A collection of my past projects
       </h2>
-      <Container>
-        <Row className="pb-3">
-          <Col>
-            <ProjectSection name="test" description='test description' projects=
-            {
-                [
-                  {
-                    name: "test",
-                    description: "test description",
-                    url: "https://github.com/Lukerd-29-00/Portfolio",
-                    demo_url: "https://lukerd-29-00.github.io/Checkers/",
-                    logo: "favicon.ico"
-                  },
-                  {
-                    name: "test2",
-                    description: "test description",
-                    url: "https://github.com/Lukerd-29-00/Portfolio",
-                    demo_url: "https://lukerd-29-00.github.io/Checkers/",
-                    logo: "favicon.ico"
-                  },
-                  {
-                    name: "test3",
-                    description: "test description",
-                    url: "https://github.com/Lukerd-29-00/Portfolio",
-                    demo_url: "https://lukerd-29-00.github.io/Checkers/",
-                    logo: "favicon.ico"
-                  }
-
-                ]
-              }
-            />
-          </Col>
-        </Row>
-        <Row className="pb-3">
-          <Col>
-            <ProjectSection name="test2" description='test description' projects=
-            {
-              [
-                {
-                  name: "test",
-                  description: "test description",
-                  url: "https://github.com/Lukerd-29-00/Portfolio",
-                  demo_url: "https://lukerd-29-00.github.io/Checkers/",
-                  logo: "favicon.ico"
-                }
-              ]
-            }
-          />
-          </Col>
-        </Row>
-        
-        
+      {
+        projects === null ? 
+        <Spinner animation="border"/>
+        :
+        <Container>
+          {projects.map((project) => {
+            return <Row className = "pb-3" key={project.name}>
+              <Col>
+                <ProjectSection {...project}/>
+              </Col>
+            </Row>
+          })}
       </Container>
+      }
     </div>
   );
 }
