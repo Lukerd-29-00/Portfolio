f09372b49be01b84867c087e94b22485476e315b
Lucas <lukerdriscoll@gmail.com>
2024-02-04 19:28:04 -0500

    changed default icon

--- src/ProjectCard.tsx
+++ src/ProjectCard.tsx
@@ -1,7 +1,6 @@
 import Card from "react-bootstrap/Card"
 import Image from "react-bootstrap/Image"
 import "./ProjectCard.css"
-import { ProjectSection } from "./ProjectSection"
 export interface ProjectCardProps{
     name: string,
     description: string
@@ -16,7 +15,7 @@ export function ProjectCard(props: ProjectCardProps): JSX.Element{
             {props.name}
         </Card.Title>
         <Card.Body id={props.demo_url !== undefined ? "link" : undefined}onClick = {props.demo_url !== undefined ? () => { window.open(props.demo_url)} : undefined}>
-            {props.logo !== undefined ? <Image src={props.logo}/> : props.demo_url !== undefined ? <iframe  src={props.url} onClick={() => {window.open(props.demo_url)}}/> : <Image src="https://avatars.githubusercontent.com/u/53152087?v=4"/>}
+            {props.logo !== undefined ? <Image style={{"maxHeight": "100px", "maxWidth" : "100px"}} src={props.logo}/> : <Image src="https://avatars.githubusercontent.com/u/53152087?v=4&s=40" height={100} width={100}/>}
         </Card.Body>
         <Card.Footer>
             {props.description}
